a2bbf2590237e4ded7d0150c5aadde59d0cc9896
Morgan Crouch <mcrouch@atlassian.com>
2022-09-03 09:36:11 +1000

    Add configuration and test cases

---
+++ test_cases/first_and_second_rule.java
@@ -0,0 +1,12 @@
+// Lines 7 & 9 are deleted when the edge is global
+package com.atlassian.rdbms.dbimportexport.runner.impl;
+
+import com.a.b.c.RolloutFlag;
+
+class SandboxHashFinder {
+    static final RolloutFlag FLAG_TO_DELETE = new RolloutFlag("TEST_FF_KEY");
+
+    public String foo(String test) {
+        bar(FLAG_TO_DELETE);
+    }
+}
---
+++ test_cases/first_rule.java
@@ -0,0 +1,8 @@
+// Line 7 is deleted when the edge is global
+package com.atlassian.rdbms.dbimportexport.runner.impl;
+
+import com.a.b.c.RolloutFlag;
+
+class SandboxHashFinder {
+    static final RolloutFlag FLAG_TO_DELETE = new RolloutFlag("TEST_FF_KEY");
+}
